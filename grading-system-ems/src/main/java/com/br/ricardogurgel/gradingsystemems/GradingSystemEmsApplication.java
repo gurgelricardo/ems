28aebdb994ea3a3f6d38e966dfef48c60c2b2685
gurgelricardo <ra24540901-5@aluno.unicesumar.edu.br>
2025-08-03 15:02:01 -0300

    criação do micro serviço de gerenciamento de notas

---
+++ grading-system-ems/src/main/java/com/br/ricardogurgel/gradingsystemems/GradingSystemEmsApplication.java
@@ -0,0 +1,13 @@
+package com.br.ricardogurgel.gradingsystemems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingSystemEmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradingSystemEmsApplication.class, args);
+    }
+
+}
---
+++ grading-system-ems/src/test/java/com/br/ricardogurgel/gradingsystemems/GradingSystemEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ricardogurgel.gradingsystemems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingSystemEmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
