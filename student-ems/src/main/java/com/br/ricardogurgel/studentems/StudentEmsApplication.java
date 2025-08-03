736696ca0b06b0532730c7cc1a338ca2ae7b13b7
gurgelricardo <ra24540901-5@aluno.unicesumar.edu.br>
2025-08-03 10:56:25 -0300

    projeto local atualizado

---
+++ student-ems/src/main/java/com/br/ricardogurgel/studentems/StudentEmsApplication.java
@@ -0,0 +1,13 @@
+package com.br.ricardogurgel.studentems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentEmsApplication.class, args);
+    }
+
+}
---
+++ student-ems/src/test/java/com/br/ricardogurgel/studentems/StudentEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ricardogurgel.studentems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentEmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
